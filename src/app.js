5e27649032e3e8c787c08f51ae3013ceb689971c
Ray Haan <rayhaan@mvp-apps.ae>
2023-12-04 22:52:07 +0400

    create items with dynamic customization

--- src/app.js
+++ src/app.js
@@ -41,67 +41,73 @@ app.get('/api/', async (req, res) => {
 	}
 });
 app.post('/api/addItem', upload.single('image'), (req, res) => {
-	// Create the data file if it doesn't exist
-	if (!fs.existsSync(dataFilePath)) {
-		fs.writeFileSync(dataFilePath, '[]');
-	}
-	const formData = req.body;
-	const file = req.file;
+    if (!fs.existsSync(dataFilePath)) {
+        fs.writeFileSync(dataFilePath, '[]');
+    }
 
-    // Validate the form data
+    const formData = req.body;
+    const file = req.file;
+
     if (!validateFormData(formData)) {
         return res.status(400).json({ error: 'Invalid form data' });
     }
 
-	// Move the uploaded file to a permanent location (you might want to handle errors more gracefully)
-	const filePath = `uploads/${file.filename}`;
-	fs.renameSync(file.path, filePath);
+    const filePath = `uploads/${file.filename}`;
+    fs.renameSync(file.path, filePath);
 
-	const newItem = {
-		name: {
-		  en: formData.name_en,
-		  ar: formData.name_ar,
-		},
-		description: {
-		  en: formData.description_en,
-		  ar: formData.description_ar,
-		},
-		price: parseFloat(formData.price),
-		image: "http://localhost:3001/" + filePath,
-		customization: [
-		  {
-			name: {
-			  en: formData.customization_name_en,
-			  ar: formData.customization_name_ar,
-			},
-			variants: [
-			  {
-				name: {
-				  en: formData.variant_name_en,
-				  ar: formData.variant_name_ar,
-				},
-				price: parseFloat(formData.variant_price),
-			  },
-			],
-		  },
-		],
-	  };
-    // Read existing data from the file
+    const newCustomizations = [];
+    let customizationIndex = 0;
+
+    // Iterate over customizations and variants dynamically
+    while (formData[`customization_name_en_${customizationIndex}`] !== undefined) {
+        const newCustomization = {
+            name: {
+                en: formData[`customization_name_en_${customizationIndex}`],
+                ar: formData[`customization_name_ar_${customizationIndex}`],
+            },
+            variants: [],
+        };
+
+        let variantIndex = 0;
+
+        while (formData[`variant_name_en_${customizationIndex}_${variantIndex}`] !== undefined) {
+            const newVariant = {
+                name: {
+                    en: formData[`variant_name_en_${customizationIndex}_${variantIndex}`],
+                    ar: formData[`variant_name_ar_${customizationIndex}_${variantIndex}`],
+                },
+                price: parseFloat(formData[`variant_price_${customizationIndex}_${variantIndex}`]),
+            };
+
+            newCustomization.variants.push(newVariant);
+            variantIndex++;
+        }
+
+        newCustomizations.push(newCustomization);
+        customizationIndex++;
+    }
+
+    const newItem = {
+        name: {
+            en: formData.name_en,
+            ar: formData.name_ar,
+        },
+        description: {
+            en: formData.description_en,
+            ar: formData.description_ar,
+        },
+        price: parseFloat(formData.price),
+        image: `http://localhost:3001/${filePath}`,
+        customizations: newCustomizations,
+    };
+
     const existingData = JSON.parse(fs.readFileSync(dataFilePath));
-
-    // Append the new data with the image URL
-    // const newData = {
-    //     ...formData,
-    //     image: "http://localhost:3001/" +filePath,
-    // };
-
     existingData.push(newItem);
-
-    // Write the updated data back to the file
     fs.writeFileSync(dataFilePath, JSON.stringify(existingData, null, 2));
 
     res.status(200).json({ message: 'Data received and saved successfully' });
 });
+
 
 const validateFormData = (formData) => {
     // Add your validation logic here
